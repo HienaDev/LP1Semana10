ce40534fb3cf8cc6fa2551c4222f7aea7acf6fa8
HienaDev <ant.rodrigues1999@gmail.com>
2023-05-19 14:46:15 +0100

    Added CompareTo method

---
+++ LootSort/Loot.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace LootSort
+{
+    /// <summary>
+    /// The Loot class should implement IComparable<Loot>
+    /// and override GetHashCode() and Equals()
+    /// </summary>
+    public class Loot : IComparable<Loot>
+    {
+        /// <summary>Type of loot.</summary>
+        public LootType Kind { get; }
+
+        /// <summary>Loot description.</summary>
+        public string Description { get; }
+
+        /// <summary>Loot value.</summary>
+        public float Value { get; }
+
+        /// <summary>
+        /// Create a new instance of loot.
+        /// </summary>
+        /// <param name="kind">Type of loot.</param>
+        /// <param name="description">Loot description.</param>
+        /// <param name="value">Loot value.</param>
+        public Loot(LootType kind, string description, float value)
+        {
+            Kind = kind;
+            Description = description;
+            Value = value;
+        }
+
+        public int CompareTo(Loot other)
+        {
+            if (other == null) return 1;
+
+            if (Kind == other.Kind)
+            {
+                if (Value == other.Value)
+                {
+                    return (string.Compare(Description, other.Description));
+                }
+                else
+                {
+                    return (other.Value - Value);
+                }
+            }
+            else
+            {
+                return (string.Compare(Kind.ToString(), other.Kind.ToString()));
+            }
+        }
+        /// <summary>
+        /// Return a nicely formatted string representing the loot instance.
+        /// </summary>
+        /// <returns>
+        /// A nicely formatted string representing the loot instance.
+        /// </returns>
+        public override string ToString() =>
+            $"[{Kind,15}]\t{Value:f2}\t{Description}";
+    }
+}
--- SamePlayer/Program.cs
+++ SamePlayer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace SamePlayer
 {
@@ -6,7 +8,20 @@ namespace SamePlayer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ICollection<Player> setOfPlayers = new HashSet<Player>();
+
+            setOfPlayers.Add(new Player(PlayerType.Tank, "Ana"));
+            setOfPlayers.Add(new Player(PlayerType.Slayer, "Paulo"));
+            setOfPlayers.Add(new Player(PlayerType.Slayer, "Paulo"));
+            setOfPlayers.Add(new Player(PlayerType.Slayer, "Ana"));
+            setOfPlayers.Add(new Player(PlayerType.Tank, "Paulo"));
+            setOfPlayers.Add(new Player(PlayerType.Tank, "Paulo"));
+            setOfPlayers.Add(new Player(PlayerType.Tank, "Ana"));
+
+            foreach (Player p in setOfPlayers)
+            {
+                Console.WriteLine($"{p.Name} is a {p.Type}");
+            }
         }
     }
 }
