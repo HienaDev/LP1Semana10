962db17c7bcb14e709b0190dad51d44c9dfc4893
HienaDev <ant.rodrigues1999@gmail.com>
2023-05-19 14:06:39 +0100

    Create collections of strings

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace StringCollections
 {
@@ -6,7 +8,36 @@ namespace StringCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ICollection<string> lista = new List<string>();
+            Stack<string> pilha = new Stack<string>();
+            Queue<string> fila = new Queue<string>();
+            ICollection<string> hash = new HashSet<string>();
+
+            lista.Add("Ola");
+            lista.Add("Adeus");
+            lista.Add("Ola");
+            lista.Add("Hello");
+            lista.Add("World");
+
+            pilha.Push("Ola");
+            pilha.Push("Adeus");
+            pilha.Push("Ola");
+            pilha.Push("Hello");
+            pilha.Push("World");
+
+            fila.Enqueue("Ola");
+            fila.Enqueue("Adeus");
+            fila.Enqueue("Ola");
+            fila.Enqueue("Hello");
+            fila.Enqueue("World");
+
+            hash.Add("Ola");
+            hash.Add("Adeus");
+            hash.Add("Ola");
+            hash.Add("Hello");
+            hash.Add("World");
+
+            
         }
     }
 }
